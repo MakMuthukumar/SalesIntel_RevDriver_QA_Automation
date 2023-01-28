40bd6f76edf673c512234ab1ea3218224877a09f
MakMuthukumar <muthumak@gmail.com>
2023-01-28 13:04:58 +0530

    Initial Commit

---
+++ src/test/java/org/runner/Runner.java
@@ -0,0 +1,51 @@
+package org.runner;
+
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.apache.commons.io.FileUtils;
+import org.base.Global;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
+
+import io.cucumber.testng.AbstractTestNGCucumberTests;
+import io.cucumber.testng.CucumberOptions;
+
+@CucumberOptions(features = "src\\test\\resources\\Features\\", glue = { "org.stepdefinition",
+		"org.runner" }, dryRun = false, monochrome = true, tags = {
+				"@Various_Region_India,@Various_Region_US" }, plugin = {
+						"com.aventstack.extentreports.cucumber.adapter.ExtentCucumberAdapter:", "pretty",
+						"io.qameta.allure.cucumber4jvm.AllureCucumber4Jvm", "html:src\\test\\resources\\Reports\\HTML",
+						"json:src\\test\\resources\\Reports\\Json\\cucumber.json",
+						"junit:src\\test\\resources\\Reports\\XML\\cucumber.xml" })
+public class Runner extends AbstractTestNGCucumberTests {
+	
+	@BeforeSuite
+	public void allureReportCleanup() throws Throwable {
+		String fileDirectoryDetails = System.getProperty("user.dir") + "\\allure-results";
+//		String targetDirectory = System.getProperty("user.dir") + "\\target";
+		File file = new File(fileDirectoryDetails);
+
+		if (file.isDirectory()) {
+			try {
+				FileUtils.cleanDirectory(file);
+				Thread.sleep(2000);
+//				FileUtils.cleanDirectory(new File(targetDirectory));
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			System.out.println("Previously generated Files Deleted Permanently");
+		}
+
+	}
+
+	@AfterSuite
+	public void cucumberReportGenerate() {
+		Global.generateJVMReport(
+				System.getProperty("user.dir") + "\\src\\test\\resources\\Reports\\Json\\cucumber.json");
+	}
+
+}
---
+++ target/test-classes/Reports/HTML/report.js
@@ -0,0 +1,225 @@
+$(document).ready(function() {var formatter = new CucumberHTML.DOMFormatter($('.cucumber-report'));formatter.uri("file:src/test/resources/Features/TC01_Login_Feature_With_Various_Region_India.feature");
+formatter.feature({
+  "name": "Validate application login Page with India Region functionality",
+  "description": "",
+  "keyword": "Feature"
+});
+formatter.scenario({
+  "name": "To validate application login page with Indian Region",
+  "description": "",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "name": "@Various_Region_India"
+    }
+  ]
+});
+formatter.before({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Launch the url with Indian Region",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "Steps.launch_the_url_with_Indian_Region()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Click on Chrome Extensions",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "Steps.click_on_Chrome_Extensions()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Click on Pin From Chrome Extensions",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "Steps.click_on_Pin_From_Chrome_Extensions()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Click on RevDriver by SalesIntel",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "Steps.click_on_RevDriver_by_SalesIntel()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Switch To Frame with Indian Region",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "Steps.switch_To_Frame_with_Indian_Region()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Enter the UserName with Indian Region",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "Steps.enter_the_UserName_with_Indian_Region()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Enter the Password with Indian Region",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "Steps.enter_the_Password_with_Indian_Region()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Click on Login CTA with Indian Region",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "Steps.click_on_Login_CTA_with_Indian_Region()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Close the Browser with Indian Region",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "Steps.close_the_Browser_with_Indian_Region()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.after({
+  "status": "passed"
+});
+formatter.uri("file:src/test/resources/Features/TC02_Login_Feature_With_Various_Region_US.feature");
+formatter.feature({
+  "name": "Validate application login Page with US Region functionality",
+  "description": "",
+  "keyword": "Feature"
+});
+formatter.scenario({
+  "name": "To validate application login page with US Region",
+  "description": "",
+  "keyword": "Scenario",
+  "tags": [
+    {
+      "name": "@Various_Region_US"
+    }
+  ]
+});
+formatter.before({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Launch the url with US Region",
+  "keyword": "Given "
+});
+formatter.match({
+  "location": "Steps.launch_the_url_with_US_Region()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Click on Chrome Extensions",
+  "keyword": "When "
+});
+formatter.match({
+  "location": "Steps.click_on_Chrome_Extensions()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Click on Pin From Chrome Extensions",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "Steps.click_on_Pin_From_Chrome_Extensions()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Click on RevDriver by SalesIntel",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "Steps.click_on_RevDriver_by_SalesIntel()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Switch To Frame with US Region",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "Steps.switch_To_Frame_with_US_Region()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Enter the UserName with US Region",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "Steps.enter_the_UserName_with_US_Region()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Enter the Password with US Region",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "Steps.enter_the_Password_with_US_Region()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Click on Login CTA with US Region",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "Steps.click_on_Login_CTA_with_US_Region()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.step({
+  "name": "Close the Browser with US Region",
+  "keyword": "And "
+});
+formatter.match({
+  "location": "Steps.close_the_Browser_with_US_Region()"
+});
+formatter.result({
+  "status": "passed"
+});
+formatter.after({
+  "status": "passed"
+});
+});
